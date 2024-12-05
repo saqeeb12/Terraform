e4369d241c66d9d52aff0aee06017afe87a697c6
SAQEEB ISMAILMAGDUM <saqeeb.ismailmagdum@kyndryl.com>
2024-12-05 12:46:56 +0530

    added sample code

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+    required_version = ">= 1.2.0"
+}
 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = ami-0866a3c8686eaeeba
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-demo"
+  }
+}
